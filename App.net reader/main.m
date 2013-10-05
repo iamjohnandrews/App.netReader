87d57d63277e1a6bfd25e35db271d5a5689927f7
John Andrews <johnandrews@Johns-MacBook.local>
2013-10-05 13:51:24 -0500

    Initial Commit

---
+++ App.net reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  App.net reader
+//
+//  Created by John Andrews on 10/5/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ App.net readerTests/App_net_readerTests.m
@@ -0,0 +1,34 @@
+//
+//  App_net_readerTests.m
+//  App.net readerTests
+//
+//  Created by John Andrews on 10/5/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface App_net_readerTests : XCTestCase
+
+@end
+
+@implementation App_net_readerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
